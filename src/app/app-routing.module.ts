5bc84a3b6fb0670f960b9ac7cf6a25e5fc70e3a7
th34n <mingtheanlay@gmail.com>
2021-10-11 20:29:37 +0700

    init msg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+import { ShowEmployeeComponent } from './components/show-employee/show-employee.component';
+import { HomeComponent } from'./components/home/home.component';
+
+const routes: Routes = [
+  { path: 'list', component: ListEmployeeComponent },
+  { path: 'show/:id', component: ShowEmployeeComponent },
+  { path: 'add', component: AddEmployeeComponent },
+  { path: 'edit/:id', component: EditEmployeeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+import { ShowEmployeeComponent } from './components/show-employee/show-employee.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent,
+    ListEmployeeComponent,
+    ShowEmployeeComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
